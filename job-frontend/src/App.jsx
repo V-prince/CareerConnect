01bf6d92db94cf68151c6c4269f9fe1a5789f2db
panchals <rishipanchal41@gmail.com>
2026-07-26 18:22:14 +0530

    navbar in app

--- job-frontend/src/App.jsx
+++ job-frontend/src/App.jsx
@@ -1,10 +1,12 @@
 import { useState } from 'react'
+import { Navbar } from './layouts/Navbar'
 
 function App() {
   
 
   return (
     <>
+    <Navbar/>
 
     {/* Here We Define Routes of our pages */}
       
